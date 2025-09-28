35ba7ef7647ecdd8cf9a12adbabeca750c12c270
saumyasingh9 <bajrangbali246new@gmail.com>
2025-09-28 18:12:46 +0530

    Python code in AWS lambda function added

--- ebs-stale-snapshots.py
+++ ebs-stale-snapshots.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
         for instance in reservation['Instances']:
             active_instance_ids.add(instance['InstanceId'])
 
-    # Iterate through each snapshot and delete if it's not attached to any volume or the volume is not attached to a running instance
+    # Iterate through each snapshot and delete if it's not attached to any volume or the volume is not attached to a running instances
     for snapshot in response['Snapshots']:
         snapshot_id = snapshot['SnapshotId']
         volume_id = snapshot.get('VolumeId')
